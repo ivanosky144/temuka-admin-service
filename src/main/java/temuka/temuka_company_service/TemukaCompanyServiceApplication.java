30a1ab74e301033c5d1c0dcb71fba3f8d4beeffd
ivanosky144 <ivanoskytan@gmail.com>
2024-09-13 18:55:38 +0700

    feat: initialize repo

---
+++ src/main/java/temuka/temuka_company_service/TemukaCompanyServiceApplication.java
@@ -0,0 +1,13 @@
+package temuka.temuka_company_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemukaCompanyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemukaCompanyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/temuka/temuka_company_service/TemukaCompanyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package temuka.temuka_company_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemukaCompanyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
